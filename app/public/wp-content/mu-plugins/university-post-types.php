7ec824744a68e4c00c0d13c8a7717a0273e4b66e
Piotrek <piotrjazwinski94@gmail.com>
2019-02-16 20:43:38 +0100

    Custom layout for Events done in a half

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,9 @@
 <?php
 function university_post_types(){
 	register_post_type('event', array(
+		'supports' => array('title', 'editor', 'excerpt'),
+		'rewrite' => array('slug' => 'events'),
+		'has_archive' => true, //it makes that when we will go to http://localhost:3000/event then we will see an archive of events
 		'public' => true, //it makes function visible in admin board 
 		'labels' => array(
 			'name' => 'Events',
